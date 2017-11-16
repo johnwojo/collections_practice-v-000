f838e1e22384c2e7d30f22cef2c84b7615b8bb46
John Wojciechowski <jwojo13@gmail.com>
2017-11-16 20:06:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.collect do |word| word + "s"
+  array.each_with_index {element,i} i == 2}.collect do |word| word + "s"
   end
-  array[2]-"s"
 end
