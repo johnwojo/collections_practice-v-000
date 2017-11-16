0abbe1788775748d74ddf3269c62a574718eb702
John Wojciechowski <jwojo13@gmail.com>
2017-11-16 18:55:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,13 +24,8 @@ end
 
 
 def kesha_maker(array)
-array.each do |word|
-end
+  array.each do |word|
+    word(3) = $
+  end
 end
 
-
-take the array of strings... make each of them their own seperate string
-then split each of those into their own array of letters
-reaplce the [2] with the dollar sign 
-then join them back up into words
-then shovel them into an array?
